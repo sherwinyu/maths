cb7edc10859a49ea36fedb8464c826917a173e88
Sherwin Yu <sherwin.x.yu@gmail.com>
2011-08-01 23:37:35 -0700

    new file:   views.py 	modified:   urls.py

---
+++ views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+def hello_world(request): #, direction, num_hrs):
+	#num_hrs = int(num_hrs)
+	#if direction=="minus":
+	    #num_hrs *= -1
+	#dt = datetime.datetime.now() #+ datetime.timedelta(hours=num_hrs)
+	#t = get_template('current_datetime.html')
+	#html = t.render(Context({'time': dt}))
+	return HttpResponse("helloworld")
